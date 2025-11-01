6de999e7df2066ac47ca3ebedc04da2ec6a51218
Leonardo Amaro da Silva <93151770+leonardo-amaro@users.noreply.github.com>
2025-11-01 00:11:23 +0000

    importando libs para API + GET

---
+++ api.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+api = Flask(__name__)
+CORS(api)
+
+@api.route("/api/vendas", methods=["GET"])
+def get_vendas():
+    dados = [
+        {"id": 1, "mes": "Jan", "vendas": 2000},
+        {"id": 2, "mes": "Fev", "vendas": 4000},
+        {"id": 2, "mes": "Mar", "vendas": 3700}
+    ]
+    return jsonify(dados)
