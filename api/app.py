d320b929a370ef5a3ce76eb2c52d06280cb97826
Leonardo Amaro da Silva <93151770+leonardo-amaro@users.noreply.github.com>
2025-11-03 22:15:02 +0000

    tentando com FastAPI

--- api/app.py
+++ api/app.py
@@ -1,32 +1,12 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
+from fastapi import FastAPI
 
-app = Flask(__name__)
-CORS(app)
+app = FastAPI()
 
-@app.route("/api/vendas", methods=["GET"])
+@app.get("/api/vendas")
 def get_vendas():
     dados = [
         {"id": 1, "mes": "Jan", "vendas": 2000},
         {"id": 2, "mes": "Fev", "vendas": 4000},
         {"id": 2, "mes": "Mar", "vendas": 3700}
     ]
-    return jsonify(dados)
-
-def handler(event, context):
-    from werkzeug.wrappers import Request, Response
-
-    @Request.application
-    def application(request):
-        with app.request_context(request.environ):
-            response = app.full_dispatch_request()
-            return response
-
-    req = Request.from_values(**event)
-    res = application(req)
-
-    return {
-        "statusCode": res.status_code,
-        "headers": dict(res.headers),
-        "body": res.get_data(as_text=True),
-    }
+    return dados
